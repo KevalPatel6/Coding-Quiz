5f9eba84439b3fc03d8eccfc4980eca52c51945c
[Keval Patel] <[khp90611@gmail.com]>
2023-08-01 15:55:48 -0400

    fixed timer and added cleared interval so it stops at Game Over or when it is 0, fixed issue with question not loading, gameOver screen renders when timer is 0, Need to do scoring and work on answer being the right value

--- script.js
+++ script.js
@@ -1,35 +1,44 @@
 let choiceListContainer = document.querySelector(".answerChoicesContainer")
-// let questionContainer = document.querySelector(".questionContainer") <<Don't need I think
+let gameOverHeader = document.querySelector("#gameOverHeader")
+// let questionContainer = document.querySelector(".header") <<Don't need I think
 let questionDisplay = document.querySelector(".questionHeader")
 let instructions = document.querySelector(".instructions")
 let startQuizButton = document.querySelector(".startQuiz")
 let timerText = document.querySelector(".timer")
 let timer = 60
-let timerID = setInterval(function(){
-    timerText.textContent= "Time: " + timer + " seconds"
-    timer -=1 
-},1000);
-let currentQuestion = 0
 let answersEl = document.querySelectorAll("button");
-let rightWrong = document.querySelector(".rightorWrong");
+let rightWrong = document.querySelector("#rightOrWrong");
 let answer1Button = document.querySelector("#answer1");
 let answer2Button = document.querySelector("#answer2");
 let answer3Button = document.querySelector("#answer3");
 let answer4Button = document.querySelector("#answer4");
-
+let submitButton = document.querySelector("#submit");
+let score = 0;
+let currentQuestion = 0
 let questionsBank = [{
 
     question: "Who is the greatest of all time?",
     choices: ['Lebron James', 'Michael Jordan', 'Kobe Bryant', 'Tim Duncan'],
     answer: 'Lebron James'
-    },
-    
-    {
-        question: "What sport do the Philadelphia Eagles play?",
-        choices: ['Football', 'Basketball', 'Baseball', 'Hockey'],
-        answer: 'Football'
-    
-    }]
+},
+
+{
+    question: "What sport do the Philadelphia Eagles play?",
+    choices: ['Football', 'Basketball', 'Baseball', 'Hockey'],
+    answer: 'Football',
+
+},
+{
+    question: "What is the color of the sky?",
+    choices: ['Red','Yellow','Green','Blue'],
+    answer: 'Blue'
+},
+{
+    question: "",
+    choices: [],
+    answer: ''
+}
+];
 
 
 init()
@@ -38,80 +47,103 @@ init()
 
 
 //Initial function when the page loads//
-function init (){
-//Everything that needs to be set on initial loading of page
+function init() {
+    //Everything that needs to be set on initial loading of page
     questionDisplay.textContent = "Coding Quiz Challenge";
     instructions.textContent = "Answer as many questions as you can in 60 seconds. Each question you answer wrong will reduce the timer by 5 seconds. Good luck!!"
-    timerText.textContent= "Time: " + timer + " seconds"
+    timerText.textContent = "Time: " + timer + " seconds"
     choiceListContainer.style.display = "none";
-    
-
-//Click event listener    
-startQuizButton.addEventListener("click", function(){
-    renderQuestion(); 
-    showAnswers();
-    
-    hideStartButton();
-    
-           
 
 
+    //Click event listener    
+    startQuizButton.addEventListener("click", function () {
+        renderQuestion();
 
-    timerText = timerID;
+        hideStartButton();
 
+         let timerID = setInterval(function () {
+            timer--
+            timerText.textContent = "Time: " + timer + " seconds"
+            if(timer===0 || currentQuestion === questionsBank.length-1){
+                clearInterval(timerID)
+                gameOver();
+            }
+        }, 1000);
+
+      
     });
-        
-//Setting the time interval to start on click of start//
-        
-}
 
-function showAnswers() {
-    choiceListContainer.style.display = "flex";
+    //Setting the time interval to start on click of start//
 
 }
+
 
 //Hiding the start button after click
-    //QUESTION: Is there an easier way to set this//
-function hideStartButton(){
+//QUESTION: Is there an easier way to set this//
+function hideStartButton() {
     startQuizButton.style.display = "none"
-    instructions.style.display ="none"
+    instructions.style.display = "none"
 };
 
 
-function renderQuestion () {  
-    questionDisplay.textContent = questionsBank[currentQuestion].question
-    answer1Button.textContent = questionsBank[currentQuestion].choices[0]
-    answer2Button.textContent = questionsBank[currentQuestion].choices[1]
-    answer3Button.textContent = questionsBank[currentQuestion].choices[2]
-    answer4Button.textContent = questionsBank[currentQuestion].choices[3]
-//Answers need to be in the form of a button
+function renderQuestion() {
+    choiceListContainer.style.display = "flex";
+    choiceListContainer.style.alignItems = "center";
+
+        questionDisplay.textContent = questionsBank[currentQuestion].question
+        answer1Button.textContent = questionsBank[currentQuestion].choices[0]
+        answer2Button.textContent = questionsBank[currentQuestion].choices[1]
+        answer3Button.textContent = questionsBank[currentQuestion].choices[2]
+        answer4Button.textContent = questionsBank[currentQuestion].choices[3]
+    
+    //Answers need to be in the form of a button
+
+}
 
 
 
-choiceListContainer.addEventListener("click", function(event){
-    if(event.target.matches("button")){
-        currentQuestion ++
+choiceListContainer.addEventListener("click", function (event) {
+    
+    
+    if (event.target.matches("button")) {
+        currentQuestion++
         renderQuestion();
     }
-    if(event.target.innerText === questionsBank[currentQuestion].answer){
+
+    if (event.target.innerText === questionsBank[currentQuestion].answer) {
         rightWrong.textContent = "Correct!"
+        score++
+}   
+    else {
+        rightWrong.textContent = "Wrong!";
+        timer -= 5;
     }
-    else{
-        rightWrong.textContent = "Wrong!"
-    }
-//HOW do I find out which one they selected? I think I got the answer but looks like I don't
-})}
 
-    
-
+//Game Over Screen Trigger//
+    // if (timer === 0 || currentQuestion === questionsBank.length-1) {
         
+    //     gameOver()
+    // }
+
+})
+
+function gameOver() {
+    questionDisplay.style.display = "none";
+    choiceListContainer.style.display = "none";
+    rightWrong.style.display = 'none';
+    gameOverHeader.textContent = "Game Over!";
+    document.getElementById("formContainer").hidden = false;
+    document.querySelector("#submit").textContent = "Submit"
+
+    submitButton.addEventListener("click")
+}
+
 //     }
 
 //To Do:
     //change CSS so the line element is smaller >>> Done
 
-    //click inside the container takes you to the next question
-        //if statement for when an actual answer is clicked
+    //click inside the container takes you to the next question >> Done
+        //if statement for when an actual answer is clicked  >>> Done
 
-    //Need an if the answer chosen equals answer then show correct, else wrong
-        
+    //Need an if the answer chosen equals answer then show correct, else wrong >>> Done
